b318a3ac04f307e8581f3d4e2f152b7500cbe36a
Samuele Sandrini <samuele.sandrini@polito.it>
2024-07-08 19:14:00 +0200

    Fix import data

--- import_data.sh
+++ import_data.sh
@@ -13,7 +13,7 @@ do
     COLLECTION_NAME="task_results_online"
 
     # Set file path (using the element and file name)
-    FILE_PATH="$element/DistanceMonitoring/hr_distance.csv"
+    FILE_PATH="$simulation_name/Distance_Monitoring/hr_distance.csv"
 
     # Check if the specified file exists
     if [ ! -f "$FILE_PATH" ]; then
